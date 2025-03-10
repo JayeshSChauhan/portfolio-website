454e958b68ffa1e1a720ffe27ce8093cc29f056e
JAYESH S CHAUHAN <135628377+JayeshSChauhan@users.noreply.github.com>
2025-03-11 00:40:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template
 app = Flask(__name__)
 
@@ -5,4 +6,6 @@ app = Flask(__name__)
 def home_page():
     return render_template('index.html')
 
-app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Use PORT from Render, default to 5000
+    app.run(host="0.0.0.0", port=port, debug=True)
